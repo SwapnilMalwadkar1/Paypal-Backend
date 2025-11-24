074f947ad86d1769ce4c8586c9716bb6cf9f5f2a
SwapnilMalwadkar1 <Swapnilmalwadkar57@gmail.com>
2025-11-24 06:37:09 +0530

    NA

--- src/main/java/com/paypal/User_service/entity/User.java
+++ src/main/java/com/paypal/User_service/entity/User.java
@@ -1,37 +1,62 @@
 package com.paypal.User_service.entity;
 
-
 import jakarta.persistence.*;
-import org.springframework.boot.autoconfigure.web.WebProperties;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Table(name = "app_user")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = IDENTITY)
-     private Long id ;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 
-     private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
 
-
-     @Column(unique = true)
-     private  String email;
-
-     private String password;
-
+    private String role;
 
 
-    public void SetId (Long id) {
+    public void setId(Long id) {
         this.id = id;
-      }
-    public Long getId() {
-      return id;
-
     }
-  }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    //getter setters
+}
