2037146d5b11a432c644760d2add6108b9cbdf59
SwapnilMalwadkar1 <Swapnilmalwadkar57@gmail.com>
2025-11-24 06:38:24 +0530

    NA

--- src/main/java/com/paypal/User_service/security/SecurityConfig.java
+++ src/main/java/com/paypal/User_service/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.paypal.user_service.security;
+package com.paypal.User_service.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
